8148982b36207f765ae89977d10f746df4f9cac7
jakovlev-fedor <jakovlev.fedor.me@gmail.com>
2020-11-03 10:47:13 +0300

    INIT COMMIT

---
+++ 01_lesson/DataTypes.py
@@ -0,0 +1,172 @@
+# 01 float
+x = 34.33
+
+# 02 str
+x = "This is string"
+
+# 03 List[float]
+x = [34.33, 43.34]
+
+# 04 Dict{float : Set[Tuple[str]]}
+x = {3.14: {('a', 'b'),
+            ('a', 'b')
+            },
+     4.15: {('a', 'b'),
+            ('a', 'b')
+            }
+     }
+
+# 05 Tuple(str, float)
+
+x= ('str', 34.34)
+
+# 06 Dict {float : float}
+
+x = {34.34: 45.45, 34.33: 45.45}
+
+# 07 Set {str, str}
+
+x = {'aa', 'ab', 'ac'}
+
+# 08 bool
+
+x = True
+
+# 09 Set {bool1, bool2}
+
+x = {True, True}
+
+# 10 Set {Tuple1 (float, int, str), Tuple1 (float, int, str), Tuple1 (float, int, str)}
+
+x = {(100.999, 100, '999'),
+     (100.999, 100, '999'),
+     (100.999, 100, '999')
+     }
+
+# 11 int
+
+x = 199
+
+# 12 List [str, str, str]
+
+x = ['er', 'we', 'qw', 'sd']
+
+# 13 Dict {Tuple(float, bool, str) : bool}
+
+x = {(23.12, True, 'sfgs'): False,
+     (23.12, True, 'sfgs'): False
+     }
+
+
+x = 1
+
+# 14 Dict {str : str}
+
+x = {'we': 'as',
+     'qw': 'sd'
+     }
+
+# 15 Tuple (int)
+
+x = (34, 32)
+
+# 16 Dict {Tuple (float, float) : List [bool, bool]}
+
+x = {(34.34, 23.23): [True, False],
+     (34.34, 23.23): [True, False]
+     }
+
+# 17 List [bool, bool]
+
+x = [True, False]
+
+# 18 Set {Any1, Any2}
+
+x = {45, 'sd', True}
+
+# 19 List [int]
+
+x = [434, 123, 454]
+
+# 20 List [Any]
+
+x = ['rafeg', 23, False, 34.45]
+
+# 21 Dict {int : List[Any]}
+
+x = {34: ['rafeg', 23, False, 34.45],
+     45: ['rafeg', 23, False, 34.45]
+     }
+
+
+# 22 Tuple (int, bool)
+
+x = (4543, True)
+
+# 23 Tuple (float, str)
+
+x = (43.23, 'sfghs')
+
+# 24 Tuple (bool)
+
+x = (True, False)
+
+# 25 Tuple (str, str)
+
+x = ('sdfg', 'dgfh')
+
+# 26 Tuple (int, bool, float)
+
+x = (344, True, 45.2)
+
+# 27 Dict {int : Set{str}}
+
+x = {34: {'rt', 'fg'},
+     54: {'rt', 'fg'}
+     }
+
+# 28 Tuple (float, float)
+
+x = (34.2, 54.2)
+
+# 29 Set {int}
+
+x = {34, 45, 56}
+
+# 30 Dict {Tuple(bool, int, int) : float}
+
+x = {(True, 45, 23)  : 23.3,
+     (False, 42, 25) : 45.7
+     }
+
+
+# 31 Dict {float : int}
+
+x = {34.5: 127,
+     45.6: 546
+     }
+
+# 32 Tuple(bool, float)
+
+x = (True, 34.5)
+
+# 33 List [Tuple(bool, float), Tuple(bool, float)]
+
+x = [(True, 56.2323),
+     (True, 56.2323)
+     ]
+
+# 34 List [Tuple(int), Tuple(int)]
+
+x = [(45, 23, 56),
+     (45, 23, 56)
+     ]
+
+# 35 Tuple (bool, str)
+
+x = (True, 'qwe')
+
+# 36 Tuple (float)
+
+x = (43.5, 34.12)
+
